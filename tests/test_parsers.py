b66f64b719d57da2ca8636192b54d0104d93134b
vsc10255 <vsc10255@login1.cerberus.os>
2020-10-16 17:16:54 +0200

    Fixed format change that caused unittest to crash.

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -7,11 +7,11 @@ methane = os.path.join(CUR_DIR, 'test_data/methane.xyz')
 def test_load_zmatrix_from_file():
     parser = ZMatrix()
     atomsCheck = ['C', 'H', 'H', 'H', 'H']
-    xyzCheck = [['0.000000', '0.000000', '0.000000'],
-    ['0.000000', '0.000000', '1.089000'],
-    ['1.026719', '0.000000', '-0.363000'],
-    ['-0.513360', '-0.889165', '-0.363000'],
-    ['-0.513360', '0.889165', '-0.363000']]
+    xyzCheck = [[0.0, 0.0, 0.0],
+    [0.0, 0.0, 1.089],
+    [1.026719, 0.0, -0.363],
+    [-0.51336, -0.889165, -0.363],
+    [-0.51336, 0.889165, -0.363]]
     
     atoms, xyz = parser.load_zmatrix_from_file(methane)
     assert atoms == atomsCheck
