0443e1396367b6f55e17cd0b06bec28759117bf6
vsc10255 <vsc10255@login1.cerberus.os>
2020-10-16 17:47:33 +0200

    Updated XYZ parser to agree with the XYZ format

--- qubit/parsers/xyz.py
+++ qubit/parsers/xyz.py
@@ -9,14 +9,23 @@ class XYZ:
         atoms = []
         coordinates = []
         with open(file, 'r') as matrix:
-            for line in matrix:
+            for line, content in enumerate(matrix):
                 n = 0
                 xyz = []
-                for word in line.split():
-                    if n == 0:
-                        atoms.append(word)
-                    elif n > 0:
-                        xyz.append(float(word))
-                    n = n + 1
+
+                # the first line contains typically the atom count
+                if line == 0:
+                    continue
+                # the second line is empty
+                elif line == 1:
+                    continue
+                else:
+                    for word in content.split():
+                        # the other lines contain the molecular data
+                        if n == 0:
+                            atoms.append(word)
+                        elif n > 0:
+                            xyz.append(float(word))
+                        n = n + 1
                 coordinates.append(xyz)
         return atoms, coordinates
--- tests/devTest.py
+++ tests/devTest.py
@@ -1,16 +1,6 @@
-from qubit.parsers.zmatrix import ZMatrix
+from qubit.parsers.xyz import XYZ
 from qubit.descriptors import CoulombMatrix
 
 cm = CoulombMatrix()
 
 print(cm.generate_coulomb_matrix('/data/brussel/102/vsc10255/qubit/tests/test_data/methane.xyz'))
-
-import os
-CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-methane = os.path.join(CUR_DIR, 'test_data/methane.xyz')
-
-parser = ZMatrix()
-
-atoms, xyz = parser.load_zmatrix_from_file(methane)
-
-print(xyz)
