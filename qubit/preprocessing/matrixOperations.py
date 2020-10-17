effc4a299eb3c554d60302a533b38a4dde4a9c0e
vsc10255 <vsc10255@login1.cerberus.os>
2020-10-17 10:51:51 +0200

    Corrected the matrix padding.

---
+++ qubit/preprocessing/matrixOperations.py
@@ -0,0 +1,15 @@
+import numpy as np
+import warnings
+
+def matrix_padding(matrix, size):
+    # calculate the wished size
+    size = size - matrix.shape[0]
+
+    # confirm the size isn't less than the default matrix size
+    if size <= 0:
+        m = matrix
+        warnings.warn('Trying to reduce the matrix size, default matrix size has been returned!', Warning)
+    else:
+        # pad the matrix
+        m = np.pad(matrix, (0, size))
+    return m
--- qubit/preprocessing/preprocessing.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def matrix_padding(matrix, size):
-    m = np.pad(matrix, (0, size))
-    return m
