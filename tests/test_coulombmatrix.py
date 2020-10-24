738e0a8e5f141b180e166a064ae9bc236b785f62
vsc10255 <vsc10255@login1.cerberus.os>
2020-10-24 12:46:38 +0200

    basic unittest for coulomb matrix

--- tests/test_coulombmatrix.py
+++ tests/test_coulombmatrix.py
@@ -8,4 +8,6 @@ methane = os.path.join(CUR_DIR, 'test_data/methane.xyz')
 def test_coulomb_matrix():
     cm = CoulombMatrix()
     m = cm.generate_coulomb_matrix(methane)
+    rm = cm.randomize_coulomb_matrix(m)
     assert np.any(m != None)
+    assert np.any(rm != None)
