4dd130d776b4937e61ee7519fa87028e99be0b0f
M.Jacobs <vsc10255@login1.cerberus.os>
2020-11-20 13:43:13 +0100

    Implemented validation of gaussian output and extraction ox optimized XYZ geometry.

--- qubit/utilities/gaussian.py
+++ qubit/utilities/gaussian.py
@@ -1,9 +1,17 @@
 import os
+import mmap
 
 class Extractor:
 
     def __init__(self, filepath):
         self.filepath = filepath
+    
+    def find_last_occurance(self, word):
+        with open(self.filepath, 'r') as f:
+            m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        i = m.rfind(str.encode(word))
+        m.seek(i)
+        return m
 
     def check_normal_execution(self):
         with open(self.filepath, 'rb') as f:
@@ -16,8 +24,34 @@ class Extractor:
             else:
                 return False
     
-    def extract_cartesian_coordinates():
-        NotImplemented
+    def extract_cartesian_coordinates(self):
+        m = self.find_last_occurance('Standard orientation')
+        m.readline()
+        m.readline()
+        m.readline()
+        m.readline()
+        m.readline()
+
+        atoms = []
+        xyz = []
+        is_molecule = True
+        while is_molecule:
+            # read and process the line
+            line = bytes.decode(m.readline())
+            split = line.split()
+
+            # check if is still the molecule
+            if len(split)==1:
+                is_molecule = False
+            else:
+                # process the line
+                atoms.append(split[1])
+                coords = []
+                coords.append(split[3])
+                coords.append(split[4])
+                coords.append(split[5])
+                xyz.append(coords)
+        return atoms, xyz
     
     def extract_HOMO_energy():
         NotImplemented
--- tests/devTest.py
+++ tests/devTest.py
@@ -1,4 +1,6 @@
 from qubit.utilities.gaussian import Extractor
 
 extractor = Extractor('/data/brussel/102/vsc10255/data/datasets/CNOS_sub1_10to20_10k/calculations/gaussian_output/JID_0.log')
-print(extractor.check_normal_execution())
+z, c = extractor.extract_cartesian_coordinates()
+print(z)
+print(c)
